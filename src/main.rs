c47db2000bd59dae73c172a94121a54eb37ac17b
Jess Frazelle <github@jessfraz.com>
2023-08-31 10:59:29 -0700

    working
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,77 @@
-use tower_lsp::{jsonrpc::Result, lsp_types::*, Client, LanguageServer, LspService, Server};
+use anyhow::{bail, Result};
+use clap::Parser;
+use slog::Drain;
+use tower_lsp::{jsonrpc::Result as RpcResult, lsp_types::*, Client, LanguageServer, LspService, Server as LspServer};
+
+lazy_static::lazy_static! {
+    // We need a slog::Logger for steno and when we export out the logs from re-exec-ed processes.
+    pub static ref LOGGER: slog::Logger = {
+        let decorator = slog_term::TermDecorator::new().build();
+        let drain = slog_term::FullFormat::new(decorator).build().fuse();
+        let drain = slog_async::Async::new(drain).build().fuse();
+        slog::Logger::root(drain, slog::slog_o!())
+    };
+}
+
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields.
+#[derive(Parser, Debug, Clone)]
+#[clap(version = clap::crate_version!(), author = clap::crate_authors!("\n"))]
+pub struct Opts {
+    /// Print debug info
+    #[clap(short, long)]
+    pub debug: bool,
+
+    /// Print logs as json
+    #[clap(short, long)]
+    pub json: bool,
+
+    #[clap(subcommand)]
+    pub subcmd: SubCommand,
+}
+
+impl Opts {
+    /// Setup our logger.
+    pub fn create_logger(&self) -> slog::Logger {
+        if self.json {
+            let drain = slog_json::Json::default(std::io::stderr()).fuse();
+            self.async_root_logger(drain)
+        } else {
+            let decorator = slog_term::TermDecorator::new().build();
+            let drain = slog_term::FullFormat::new(decorator).build().fuse();
+            self.async_root_logger(drain)
+        }
+    }
+
+    fn async_root_logger<T>(&self, drain: T) -> slog::Logger
+    where
+        T: slog::Drain + Send + 'static,
+        <T as slog::Drain>::Err: std::fmt::Debug,
+    {
+        let level = if self.debug {
+            slog::Level::Debug
+        } else {
+            slog::Level::Info
+        };
+
+        let level_drain = slog::LevelFilter(drain, level).fuse();
+        let async_drain = slog_async::Async::new(level_drain).build().fuse();
+        slog::Logger::root(async_drain, slog::o!())
+    }
+}
+
+#[derive(Parser, Debug, Clone)]
+pub enum SubCommand {
+    Server(Server),
+}
+
+/// A subcommand for running the server.
+#[derive(Parser, Clone, Debug)]
+pub struct Server {
+    /// IP address and port that the server should listen
+    #[clap(short, long, default_value = "0.0.0.0:8080")]
+    pub address: String,
+}
 
 #[derive(Debug)]
 struct Backend {
@@ -7,7 +80,7 @@ struct Backend {
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+    async fn initialize(&self, _: InitializeParams) -> RpcResult<InitializeResult> {
         Ok(InitializeResult::default())
     }
 
@@ -15,16 +88,18 @@ impl LanguageServer for Backend {
         self.client.log_message(MessageType::INFO, "server initialized!").await;
     }
 
-    async fn shutdown(&self) -> Result<()> {
+    async fn shutdown(&self) -> RpcResult<()> {
         Ok(())
     }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
     let (service, socket) = LspService::new(|client| Backend { client });
-    Server::new(stdin, stdout, socket).serve(service).await;
+    LspServer::new(stdin, stdout, socket).serve(service).await;
+
+    Ok(())
 }
