bfd52344dbb141f87e8363b864ed2b7df30630cf
Jess Frazelle <github@jessfraz.com>
2023-08-31 12:17:49 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -21,7 +21,7 @@ import {
   TextEdit,
   Selection,
   Uri,
-} from "vscode";
+} from 'vscode'
 
 import {
   Disposable,
@@ -29,68 +29,59 @@ import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
-} from "vscode-languageclient/node";
+} from 'vscode-languageclient/node'
 
-let client: LanguageClient;
+let client: LanguageClient
 // type a = Parameters<>;
 
 export async function activate(context: ExtensionContext) {
-  let disposable = commands.registerCommand("helloworld.helloWorld", async uri => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    const url = Uri.parse('/home/victor/Documents/test-dir/nrs/another.nrs')
-    let document = await workspace.openTextDocument(uri);
-    await window.showTextDocument(document);
-
-    // console.log(uri)
-    window.activeTextEditor.document
-    let editor = window.activeTextEditor;
-    let range = new Range(1, 1, 1, 1)
-    editor.selection = new Selection(range.start, range.end);
-  });
-
-  context.subscriptions.push(disposable);
-
-  const traceOutputChannel = window.createOutputChannel("Nrs Language Server trace");
-  const command = process.env.SERVER_PATH || "nrs-language-server";
+  const traceOutputChannel = window.createOutputChannel(
+    'KCL Language Server trace'
+  )
+  const command = process.env.SERVER_PATH || 'kcl-language-server'
   const run: Executable = {
     command,
     options: {
       env: {
         ...process.env,
         // eslint-disable-next-line @typescript-eslint/naming-convention
-        RUST_LOG: "debug",
+        RUST_LOG: 'debug',
       },
     },
-  };
+  }
   const serverOptions: ServerOptions = {
     run,
     debug: run,
-  };
+  }
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
   // Options to control the language client
   let clientOptions: LanguageClientOptions = {
     // Register the server for plain text documents
-    documentSelector: [{ scheme: "file", language: "nrs" }],
+    documentSelector: [{ scheme: 'file', language: 'kcl' }],
     synchronize: {
       // Notify the server about file changes to '.clientrc files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+      fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
     },
     traceOutputChannel,
-  };
+  }
 
   // Create the language client and start the client.
-  client = new LanguageClient("nrs-language-server", "nrs language server", serverOptions, clientOptions);
+  client = new LanguageClient(
+    'kcl-language-server',
+    'kcl language server',
+    serverOptions,
+    clientOptions
+  )
   // activateInlayHints(context);
-  client.start();
+  client.start()
 }
 
 export function deactivate(): Thenable<void> | undefined {
   if (!client) {
-    return undefined;
+    return undefined
   }
-  return client.stop();
+  return client.stop()
 }
 
 export function activateInlayHints(ctx: ExtensionContext) {
@@ -99,11 +90,11 @@ export function activateInlayHints(ctx: ExtensionContext) {
     updateHintsEventEmitter: new EventEmitter<void>(),
 
     async onConfigChange() {
-      this.dispose();
+      this.dispose()
 
-      const event = this.updateHintsEventEmitter.event;
+      const event = this.updateHintsEventEmitter.event
       // this.hintsProvider = languages.registerInlayHintsProvider(
-      //   { scheme: "file", language: "nrs" },
+      //   { scheme: "file", language: "kcl" },
       //   // new (class implements InlayHintsProvider {
       //   //   onDidChangeInlayHints = event;
       //   //   resolveInlayHint(hint: InlayHint, token: CancellationToken): ProviderResult<InlayHint> {
@@ -153,20 +144,31 @@ export function activateInlayHints(ctx: ExtensionContext) {
       // );
     },
 
-    onDidChangeTextDocument({ contentChanges, document }: TextDocumentChangeEvent) {
+    onDidChangeTextDocument({
+      contentChanges,
+      document,
+    }: TextDocumentChangeEvent) {
       // debugger
       // this.updateHintsEventEmitter.fire();
     },
 
     dispose() {
-      this.hintsProvider?.dispose();
-      this.hintsProvider = null;
-      this.updateHintsEventEmitter.dispose();
+      this.hintsProvider?.dispose()
+      this.hintsProvider = null
+      this.updateHintsEventEmitter.dispose()
     },
-  };
+  }
 
-  workspace.onDidChangeConfiguration(maybeUpdater.onConfigChange, maybeUpdater, ctx.subscriptions);
-  workspace.onDidChangeTextDocument(maybeUpdater.onDidChangeTextDocument, maybeUpdater, ctx.subscriptions);
+  workspace.onDidChangeConfiguration(
+    maybeUpdater.onConfigChange,
+    maybeUpdater,
+    ctx.subscriptions
+  )
+  workspace.onDidChangeTextDocument(
+    maybeUpdater.onDidChangeTextDocument,
+    maybeUpdater,
+    ctx.subscriptions
+  )
 
-  maybeUpdater.onConfigChange().catch(console.error);
+  maybeUpdater.onConfigChange().catch(console.error)
 }
