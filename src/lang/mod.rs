ec91130b47f9b56e3b6366dfbaf4e257dc3d8178
Jess Frazelle <github@jessfraz.com>
2023-08-31 16:21:30 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -3,8 +3,6 @@
 use kcl_lib::executor::SourceRange;
 use tower_lsp::lsp_types::Position;
 
-pub mod semantic_tokens;
-
 fn source_range_to_position(code: &str, source_range: SourceRange) -> Position {
     // Calculate the line and column of the error from the source range.
     let line = code[..source_range.0[0]].lines().count();
