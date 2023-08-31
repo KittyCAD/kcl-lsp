e991f7fbf7f6383c98bad9a9930ee0110fb8adf2
Jess Frazelle <github@jessfraz.com>
2023-08-31 11:11:02 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,6 +1,10 @@
 //! Functions for the `kcl` lsp server.
 
 use anyhow::Result;
+use signal_hook::{
+    consts::{SIGINT, SIGTERM},
+    iterator::Signals,
+};
 use tower_lsp::{jsonrpc::Result as RpcResult, lsp_types::*, Client, LanguageServer, LspService, Server as LspServer};
 
 /// The lsp server backend.
@@ -27,6 +31,27 @@ impl LanguageServer for Backend {
 
 /// Run the `kcl` lsp server.
 pub async fn run(opts: &crate::Server) -> Result<()> {
+    // For Cloud run & ctrl+c, shutdown gracefully.
+    // "The main process inside the container will receive SIGTERM, and after a grace period,
+    // SIGKILL."
+    // Regsitering SIGKILL here will panic at runtime, so let's avoid that.
+    let mut signals = Signals::new([SIGINT, SIGTERM])?;
+
+    tokio::spawn(enclose! { (api_context) async move {
+        for sig in signals.forever() {
+            info!("received signal: {:?}", sig);
+            info!("triggering cleanup...");
+
+            // Run the cleanup job.
+            if let Err(e) = crate::server::endpoints::do_cleanup(&api_context).await {
+                sentry::integrations::anyhow::capture_anyhow(&e);
+            }
+            // Exit the process.
+            info!("all clean, exiting!");
+            std::process::exit(0);
+        }
+    }});
+
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
