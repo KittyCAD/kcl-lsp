c64beb982960e2c7b2f0d8ef100c77d9420d084b
Jess Frazelle <github@jessfraz.com>
2023-09-02 21:56:41 -0700

    new stuff
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

---
+++ client/src/main.ts
@@ -0,0 +1,68 @@
+import * as vscode from "vscode"
+import * as lc from "vscode-languageclient/node"
+
+import { type CommandFactory, Ctx, fetchWorkspace } from "./ctx"
+import { setContextValue } from "./util"
+
+const KCL_PROJECT_CONTEXT_NAME = "inKclProject"
+
+export interface KclAnalyzerExtensionApi {
+    readonly client?: lc.LanguageClient;
+}
+
+export async function deactivate() {
+    await setContextValue(KCL_PROJECT_CONTEXT_NAME, undefined)
+}
+
+export async function activate(
+    context: vscode.ExtensionContext,
+): Promise<KclAnalyzerExtensionApi> {
+    const ctx = new Ctx(context, createCommands(), fetchWorkspace())
+    // VS Code doesn't show a notification when an extension fails to activate
+    // so we do it ourselves.
+    const api = await activateServer(ctx).catch((err) => {
+        void vscode.window.showErrorMessage(
+            `Cannot activate kcl-language-server extension: ${err.message}`,
+        )
+        throw err
+    })
+    await setContextValue(KCL_PROJECT_CONTEXT_NAME, true)
+    return api
+}
+
+async function activateServer(ctx: Ctx): Promise<KclAnalyzerExtensionApi> {
+    await ctx.start()
+    return ctx
+}
+
+function createCommands(): Record<string, CommandFactory> {
+    return {
+        restartServer: {
+            enabled: (ctx) => async () => {
+                await ctx.restart()
+            },
+            disabled: (ctx) => async () => {
+                await ctx.start()
+            },
+        },
+        startServer: {
+            enabled: (ctx) => async () => {
+                await ctx.start()
+            },
+            disabled: (ctx) => async () => {
+                await ctx.start()
+            },
+        },
+        stopServer: {
+            enabled: (ctx) => async () => {
+                // FIXME: We should re-use the client, that is ctx.deactivate() if none of the configs have changed
+                await ctx.stopAndDispose()
+                ctx.setServerStatus({
+                    health: "stopped",
+                })
+            },
+            disabled: (_) => async () => {},
+        },
+
+    }
+}
