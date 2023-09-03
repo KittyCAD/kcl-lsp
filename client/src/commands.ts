b504fe54b1d9234127e084ab1cf4768625996fda
Jess Frazelle <github@jessfraz.com>
2023-09-02 22:08:31 -0700

    fixes
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

---
+++ client/src/commands.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode'
+
+import { type Cmd, type CtxInit } from './ctx'
+import { spawnSync } from 'child_process'
+
+export function serverVersion(ctx: CtxInit): Cmd {
+  return async () => {
+    if (!ctx.serverPath) {
+      void vscode.window.showWarningMessage(
+        `kcl-language-server server is not running`
+      )
+      return
+    }
+    const { stdout } = spawnSync(ctx.serverPath, ['--version'], {
+      encoding: 'utf8',
+    })
+    const versionString = stdout.slice(`kcl-language-server `.length).trim()
+
+    void vscode.window.showInformationMessage(
+      `kcl-language-server version: ${versionString}`
+    )
+  }
+}
