c9859451615c2cf5876a5870a98f4139780fe548
Jess Frazelle <github@jessfraz.com>
2023-09-04 17:20:45 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- release/src/build.rs
+++ release/src/build.rs
@@ -76,7 +76,7 @@ impl Build {
 }
 
 fn build_client(sh: &Shell, version: &str, release_tag: &str, target: &Target) -> anyhow::Result<()> {
-    let bundle_path = Path::new("client").join("server");
+    let bundle_path = Path::new("server");
     sh.create_dir(&bundle_path)?;
     sh.copy_file(&target.server_path, &bundle_path)?;
     if let Some(symbols_path) = &target.symbols_path {
