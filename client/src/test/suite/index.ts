12d6e949d52c97df358f66e269ca298f76df5f4b
Jess Frazelle <github@jessfraz.com>
2024-06-19 20:18:46 -0700

    update
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- client/src/test/suite/index.ts
+++ client/src/test/suite/index.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 import * as Mocha from "mocha";
-import * as glob from "glob";
+const { glob } = require("glob");
 
 export function run(): Promise<void> {
   // Create the mocha test
@@ -11,7 +11,7 @@ export function run(): Promise<void> {
   const testsRoot = path.resolve(__dirname, "..");
 
   return new Promise((c, e) => {
-    glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
+    glob("**/**.test.js", { cwd: testsRoot }).then((err: any, files: string[]) => {
       if (err) {
         return e(err);
       }
